4b520aa43eae11e5ff9dfa3792dee3832830c063
SuparnaShil <suparnashil71.ssc@gmail.com>
2021-01-27 12:14:57 +0600

    Check box handling

--- NewDemo/src/CheckBox.java
+++ NewDemo/src/CheckBox.java
@@ -1,8 +1,21 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class CheckBox {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		//This is test
+		System.setProperty("webdriver.chrome.driver", "E:\\\\Selenium file\\\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.spicejet.com/");
+		System.out.println(driver.findElement(By.cssSelector("input[id*='friendsandfamily']")).isSelected());
+		driver.findElement(By.cssSelector("input#ctl00_mainContent_chk_friendsandfamily")).click();
+		System.out.println(driver.findElement(By.cssSelector("input[id*='friendsandfamily']")).isSelected());
+		
+		//total count of check box
+		System.out.println(driver.findElements(By.cssSelector("input[type='checkbox']")).size());
 
 	}
 
